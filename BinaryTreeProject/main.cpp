202eae33a2bbf30bf8b073e4a013741398aa82ee
UPSALAB <a2022111958@estudiantes.upsa.edu.bo>
2024-03-02 10:19:35 -0400

    Main con solo el arbol instanciado

--- BinaryTreeProject/main.cpp
+++ BinaryTreeProject/main.cpp
@@ -4,14 +4,6 @@
 int main() {
 
     BinaryTree tree;
-    tree.Insert(51);
-    tree.Insert(36);
-
-
-    std::cout << "Recorrido en Pre-Orden:" << std::endl;
-    tree.PreOrder(); std::cout<<std::endl;
-    std::cout << "Recorrido en In-Orden:" << std::endl;
-    tree.InOrder();
 
 
     return 0;
