96c62db68092883678d5a567d10f52c107fee51e
UPSALAB <a2022111958@estudiantes.upsa.edu.bo>
2024-03-02 09:08:45 -0400

    Test commit

--- BinaryTreeProject/cmake-build-debug/cmake_install.cmake
+++ BinaryTreeProject/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/dylan_tmut0bm/Documents/ClionProyects/Labs ADE/BinaryTreeProject
+# Install script for directory: C:/Users/UPSALAB/CLionProjects/Advanced-Data-Structure-Labs/BinaryTreeProject
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.3.3/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2023.2.2/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/dylan_tmut0bm/Documents/ClionProyects/Labs ADE/BinaryTreeProject/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/UPSALAB/CLionProjects/Advanced-Data-Structure-Labs/BinaryTreeProject/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
